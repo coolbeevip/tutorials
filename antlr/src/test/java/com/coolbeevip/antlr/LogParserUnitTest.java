1f06af29bbe97013ad41c465ea5d17acbd012201
zhanglei <zhanglei@apache.org>
2021-01-04 16:13:39 +0800

    init

--- antlr/src/test/java/com/coolbeevip/antlr/LogParserUnitTest.java
+++ antlr/src/test/java/com/coolbeevip/antlr/LogParserUnitTest.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 
 public class LogParserUnitTest {
