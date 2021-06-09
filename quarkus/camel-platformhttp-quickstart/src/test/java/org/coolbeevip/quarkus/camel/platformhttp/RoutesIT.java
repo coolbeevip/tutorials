e3daf03267d7dc06506eff5d572c257a8705dfb7
Lei Zhang <zhanglei@apache.org>
2021-06-09 22:25:00 +0800

    Remove quarkus module

--- quarkus/camel-platformhttp-quickstart/src/test/java/org/coolbeevip/quarkus/camel/platformhttp/RoutesIT.java
+++
@@ -1,8 +0,0 @@
-package org.coolbeevip.quarkus.camel.platformhttp;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class RoutesIT extends RoutesTest {
-
-}
--- quarkus/vertx-quickstart/src/main/resources/META-INF/resources/streaming.js
+++
@@ -1,7 +0,0 @@
-var eventSource = new EventSource("/hello/Quarkus/streaming");
-eventSource.onmessage = function (event) {
-  var container = document.getElementById("container");
-  var paragraph = document.createElement("p");
-  paragraph.innerHTML = event.data;
-  container.appendChild(paragraph);
-};
