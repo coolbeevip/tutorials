40b61f4ad04131587595fc006b55731bf8ea52ff
Lei Zhang <zhanglei@apache.org>
2022-12-19 10:14:14 +0800

    Add Decorator Pattern

--- design-patterns/src/main/java/com/coolbeevip/design/patterns/structural/decorator/Application.java
+++ design-patterns/src/main/java/com/coolbeevip/design/patterns/structural/decorator/Application.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 
 public class Application {
   public static void main(String[] args) throws IOException {
+    // 原始组件
     Component component = new ConcreteComponent();
+
+    // 加密装饰器
     Component encryptionDecorator = new DecoratorEncryption(component);
     encryptionDecorator.writeData("hello".getBytes());
     System.out.println(new String(encryptionDecorator.readData()));
 
 
+    // 压缩装饰器
     Component compressionDecorator = new DecoratorCompression(encryptionDecorator);
     compressionDecorator.writeData("hello".getBytes());
     System.out.println(new String(compressionDecorator.readData()));
