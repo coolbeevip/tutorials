3060d65a21b823b21a4921b0b19ded98d1557080
Lei Zhang <zhanglei@apache.org>
2023-10-25 16:02:51 +0800

    Add test case

--- redis/lettuce/src/test/java/com/coolbeevip/redis/lettuce/LettuceTest.java
+++ redis/lettuce/src/test/java/com/coolbeevip/redis/lettuce/LettuceTest.java
@@ -1,8 +1,16 @@
 package com.coolbeevip.redis.lettuce;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.protocol.ProtocolVersion;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -15,5 +23,19 @@ public class LettuceTest {
     assertThat(redisURI.getSentinelMasterId(), Matchers.is("masterId"));
     assertThat(redisURI.getSentinels().get(0).getHost(), Matchers.is("h1"));
     assertThat(String.valueOf(redisURI.getPassword()), Matchers.is("abc@#123"));
+  }
+
+  @Test
+  public void connectTestWithProtocolVersion() throws UnsupportedEncodingException {
+    String password = URLEncoder.encode("fs-sat-ctgcache-01#SSew1_DHwe23r", "UTF-8");
+    ClientOptions options = ClientOptions.builder()
+        .protocolVersion(ProtocolVersion.RESP2).build();
+    RedisURI redisURI = RedisURI.create("redis://"+password+"@10.34.24.65:31046/20");
+    RedisClient redisClient = RedisClient.create(redisURI);
+    redisClient.setOptions(options);
+    StatefulRedisConnection<String, String> connection = redisClient.connect();
+    RedisCommands<String, String> syncCommands = connection.sync();
+    String infoString = syncCommands.info();
+    System.out.println(infoString);
   }
 }
